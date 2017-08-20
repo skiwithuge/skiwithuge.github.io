037537cb0ae731383501c14c8fd53ab2cbd5ab10
skiwithuge <rasciky@gmail.com>
2017-08-20 15:47:48 +0200

    new toggle button, removed timeline library, element layout

---
+++ js/actions.js
@@ -0,0 +1,8 @@
+function togglebtn(divId) {
+    $('.togglebtn').toggle();
+    $("#" + divId).toggle('slow');
+    //$('#timelineopen').attr('class', 'show');;
+};
+$( document ).ready(function() {
+    //$('#timelineopen').hide();
+})
