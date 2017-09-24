62a731ada8c50e7269066ed5483e69a0727c9b01
skiwithuge <rasciky@gmail.com>
2017-09-24 16:04:45 +0200

    soft SEO

--- generator.php
+++ generator.php
@@ -5,8 +5,10 @@ include("functions.php") ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <title>I AM Orazio Scicolone</title>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <meta name=viewport content="width=device-width, initial-scale=1">
+  <meta name="description" content="Orazio Scicolone Personal Resume Site">
+  <title>Orazio Scicolone Site - Software Developer</title>
   <link rel="stylesheet" href="css/spectre.min.css" />
   <link rel="stylesheet" href="css/spectre-exp.min.css" />
   <link rel="stylesheet" href="css/spectre-icons.min.css" />
